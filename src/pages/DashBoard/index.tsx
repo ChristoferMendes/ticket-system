0e29ba08de4b3e0167f8df75abe110e4d1beaba3
ChristoferMendes <christoferluizdsm@gmail.com>
2022-10-05 17:04:56 -0300

    dashboard base created

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -1,16 +1,76 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch, FiUser } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { Container } from '../../components/Title/styles';
 import { AuthContext } from '../../contexts/auth'
+import { Content } from '../Profile/styles';
+
+import { DashboardContainer, Table, TicketLink } from './styles'
 
 function Dashboard() {
   const { signOut } = useContext(AuthContext);
+  const [tickets, setTickets] = useState([1])
 
   return (
-    <div>
+    <>
       <Header />
-      <div>Dashboard</div>
-      <button onClick={() => signOut()}>Leave account</button>
-    </div>
+      <Content>
+        <Title name='Dashboard'>
+          <FiMessageSquare size={25} />
+        </Title>
+        {!tickets.length ? (
+          <Container className='dashboard'>
+            <DashboardContainer>
+              <span>There's none tickets</span>
+              <TicketLink to={'/new'} className='new-ticket'>
+                <FiPlus size={25} />
+                New ticket
+              </TicketLink>
+            </DashboardContainer>
+          </Container>
+        ) : (
+          <>
+            <TicketLink to={'/new'}>
+              <FiPlus size={25} />
+              New ticket
+            </TicketLink>
+
+            <Table>
+              <thead>
+                <tr>
+                  <th scope='col'>Client</th>
+                  <th scope='col'>Subject</th>
+                  <th scope='col'>Status</th>
+                  <th scope='col'>Registered in</th>
+                  <th scope='col'>#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label='Client'>Chris</td>
+                  <td data-label='Subject'>Support</td>
+                  <td data-label='Status'>
+                    <span style={{backgroundColor: '#5cb85c'}}>Open</span>
+                  </td>
+                  <td data-label='Registered'>20/08/2022</td>
+                  <td data-label='#'>
+                    <button style={{backgroundColor: '#3583f6'}}>
+                      <FiSearch size={17}/>
+                    </button>
+                    <button style={{backgroundColor: '#f6a935'}} >
+                      <FiEdit2 size={17} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </>
+        )}
+      </Content>
+    </>
   )
 }
 
---
+++ src/pages/DashBoard/styles.ts
@@ -0,0 +1,144 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  
+`;
+
+export const DashboardContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  span {
+    margin: 2em 0;
+    font-weight: 600;
+    font-size: 1.4em;
+  }
+
+`
+
+export const Table = styled.table`
+  /* background-color: red; */
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  table-layout: fixed;
+
+  caption {
+    font-size: 1.5em;
+    margin: .5em 0 .75em;
+  }
+
+  tr {
+    background-color: #f8f8f8;
+    border: 1px solid #ddd;
+    padding: .35em;
+  }
+  
+  th, td {
+    padding: .62em;
+    text-align: center;
+  }
+
+  th {
+    font-size: .85em;
+    letter-spacing: .2em;
+    text-transform: uppercase;
+  }
+
+  td button {
+    border: 0;
+    padding: 5px;
+    margin-right: 2px;
+    align-items: center;
+    display: inline-block;
+    border-radius: 4px;
+
+    svg {
+      vertical-align: middle;
+    }
+  }
+
+  td span {
+    padding: 3px 20px;
+    border-radius: 3px;
+    color: #fff;
+  }
+
+  @media screen and (max-width: 600px){
+    border: 0;
+
+    caption {
+      font-size: 1.3em;
+    }
+
+    thead {
+      border: none;
+      clip: rect(0, 0, 0, 0);
+      height: 1px;
+      margin: -1px;
+      overflow: hidden;
+      padding: 0;
+      position: absolute;
+      width: 1px;
+    }
+
+    tr {
+      border-bottom: 3px solid #ddd;
+      display: block;
+      margin-bottom: .65em;
+    }
+
+    td {
+      border-bottom: 1px solid #ddd;
+      display: block;
+      font-size: .8em;
+      text-align: right;
+
+      &:before {
+        content: attr(data-label);
+        float: left;
+        font-weight: bold;
+        text-transform: uppercase;
+      }
+
+      &:last-child {
+        border-bottom: 0;
+      }
+    }
+  }
+
+  
+`
+
+export const TicketLink = styled(Link)`
+    
+    float: right;
+    margin-bottom: 1.5em;
+    background-color: #83bf02;
+    color: white;
+    border: 0;
+    padding: .5em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 600;
+    font-size: 1.4em;
+    border-radius: 6px;
+    transition: ease-in .2s;
+
+    svg {
+      margin-right: 5px;
+    }
+
+    &:hover {
+      background-color: #5fd204;
+      transform: scale(1.1)
+    }
+
+`
